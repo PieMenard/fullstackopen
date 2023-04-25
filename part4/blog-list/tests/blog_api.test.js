8140f3a79a29d2f753081ebc665b66d94c8060de
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-04-25 16:06:38 -0400

    complete 4.10 blog list tests step3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -20,6 +27,30 @@ test("unique identifier property of the blog posts is named id", async () => {
     expect(id).toBeDefined();
   }
 });
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "The whiteness of the whale",
+    author: "herman Melville",
+    url: "mobydick.com",
+    likes: 55
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const title = blogsAtEnd.map(blog => blog.title)
+
+  expect(title).toContain(
+    'The whiteness of the whale'
+  )
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
