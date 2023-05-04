8f2b1183d8d1eb52cd627911df5ad939a3eebaee
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-05-04 16:25:37 -0400

    complete 4.15-4.16 bloglist expansion step3-4

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,11 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -24,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
