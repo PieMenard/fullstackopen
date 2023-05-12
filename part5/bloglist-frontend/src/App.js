73e115487c0fb08ea35b69ac412b5e345b9467e3
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-05-12 15:19:01 -0400

    complete 5.5 Blog list frontend step5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [errorMessage, setErrorMessage] = useState(null)
   const [notification, setNotification] = useState(null);
   const [user, setUser] = useState(null)
+  const blogFormRef = useRef()
   
-
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -64,6 +66,7 @@ const App = () => {
         url,
       })
       setBlogs(blogs.concat(blog))
+      blogFormRef.current.toggleVisibility()
       setNotification(`Added a new blog: "${blog.title}" by "${blog.author}" `) ;
         setTimeout(() => {setNotification(null)}, 5000);
     } catch (exception) {
@@ -107,6 +110,12 @@ const App = () => {
     )
   }
 
+  const blogForm = () => (
+    <Togglable buttonLabel='new blog' ref={blogFormRef}>
+      <BlogForm handleAddBlog={handleAddBlog} />
+    </Togglable>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
@@ -117,9 +126,7 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>
       </p>
       <h2>create new blog</h2>
-      <div>
-        <BlogForm handleAddBlog={handleAddBlog}/>
-      </div>
+      <div> {blogForm()} </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
