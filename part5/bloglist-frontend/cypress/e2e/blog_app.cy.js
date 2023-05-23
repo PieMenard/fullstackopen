18c8e0f41d7a762a911c226ebfb16c53aecaff07
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-05-23 15:30:44 -0400

    complete 5.18: bloglist end to end testing step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,12 +1,40 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'Cy Test User',
+            username: 'cy_test_user',
+            password: 'test'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users',user)
         cy.visit('http://localhost:3000')
     })
 
     it('Login form is shown', function() {
-        cy.contains('ogin')
+        cy.contains('login')
         cy.contains('username')
         cy.contains('password')
+    })
+    it('user can log in', function() {
+        cy.contains('login').click()
+        cy.get('#username').type('cy_test_user')
+        cy.get('#password').type('test')
+        cy.get('#login-button').click()
+
+        cy.contains('Cy Test User logged in')
+    })
+
+    it('login fails with wrong password', function() {
+        cy.contains('login').click()
+        cy.get('#username').type('cy_test_user')
+        cy.get('#password').type('wrong')
+        cy.get('#login-button').click()
+
+        cy.get('.error')
+            .should('contain', 'Wrong credentials')
+            .and('have.css', 'color', 'rgb(255, 0, 0)')
+            .and('have.css', 'border-style', 'solid')
+
+        cy.get('html').should('not.contain', 'Cy Test User logged in')
     })
 })
