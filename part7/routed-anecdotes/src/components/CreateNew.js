0d676ad2fa02cabefd2a057d34d61aeb9f4cfa80
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-06-09 15:21:22 -0400

    complete 7.6 anecdotes and hooks step3

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -2,41 +2,48 @@ import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks/index'
 
 const CreateNew = (props) => {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const content = useField('text');
-    const author = useField('text');
-    const info = useField('text');
+  const { reset: resetContent ,...content} = useField('content')
+  const { reset: resetAuthor ,...author} = useField('author')
+  const { reset: resetInfo ,...info} = useField('info')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+    navigate('/anecdotes')
+  }
+
+  const handleReset = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
   
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      props.addNew({
-        content: content.value,
-        author: author.value,
-        info: info.value,
-        votes: 0
-      })
-      navigate('/anecdotes')
-    }
-  
-    return (
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            content
-            <input {...content} />
-          </div>
-          <div>
-            author
-            <input {...author} />
-          </div>
-          <div>
-            url for more info
-            <input {...info} />
-          </div>
-          <button>create</button>
-        </form>
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button>create</button>
+      </form>
+      <button onClick={() => handleReset()}>reset</button>
       </div>
     )
   
