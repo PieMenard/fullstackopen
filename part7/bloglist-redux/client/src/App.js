32ae06ee4ad1f1f6548895e4a41fabbdc88681ed
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-07-01 12:55:04 -0400

    complete 7.13 Redux step4 attempt 1

--- part7/bloglist-redux/client/src/App.js
+++ part7/bloglist-redux/client/src/App.js
@@ -1,70 +1,15 @@
-import { useState, useEffect} from "react";
-import { useDispatch } from "react-redux";
-import blogService from "./services/blogs";
-import loginService from "./services/login";
-import LoginForm from "./components/LoginForm";
-
-import Notification from './components/Notification'
-import { setNotification } from './reducers/notificationReducer'
 
 import BlogList from "./components/BlogList";
+import LoginForm from "./components/LoginForm";
+import Notification from "./components/Notification";
 
 const App = () => {
 
-  const [user, setUser] = useState(null)
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
-    if (loggedUserJSON) {
-        const user = JSON.parse(loggedUserJSON)
-        setUser(user)
-        blogService.setToken(user.token)
-    }
-}, [])
-
-  const handleLogin = async (username, password) => {
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      });
-
-      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
-
-      blogService.setToken(user.token);
-      dispatch(setUser(user));
-      dispatch(setNotification(`Logging successful! Hello ${user.name}! `));
-   
-    } catch (error) {
-      dispatch(setNotification("Wrong credentials"));
-    }
-  };
-
-  const handleLogout = async () => {
-    window.localStorage.clear();
-    dispatch(setUser(null));
-  };
-
-
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <LoginForm handleLogin={handleLogin} />
-      </div>
-    );
-  }
-  
   return (
     <div>
       <h2>blogs</h2>
-      <Notification />
-      <p>
-        <span>{user.name} logged in</span>
-        <button onClick={handleLogout}>logout</button>
-      </p>
+      <Notification/>
+      <LoginForm />
       <BlogList/>
     </div>
   );
--- part7/bloglist-redux/client/src/components/LoginForm.js
+++ part7/bloglist-redux/client/src/components/LoginForm.js
@@ -1,23 +1,61 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../reducers/userReducer'
+import { setNotification } from "../reducers/notificationReducer";
+import loginService from "../services/login";
 
-const LoginForm = ({ handleLogin }) => {
+const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.user)
+  const dispatch = useDispatch();
 
-  const onSubmit = (event) => {
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      dispatch(setUser(user));
+    }
+  }, [dispatch]);
+
+
+  const handleLoginFormSubmit = async (event) => {
     event.preventDefault();
-    handleLogin(username, password);
-    setUsername("");
-    setPassword("");
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      dispatch(setUser(user));
+      dispatch(setNotification(`welcome ${user.name}`,5));
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log(exception);
+      dispatch(setNotification("wrong username or password",5));
+    }
   };
 
-  LoginForm.propTypes = {
-    handleLogin: PropTypes.func.isRequired,
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogappUser");
+    dispatch(setUser(null));
+    dispatch(setNotification("logged out",5));
   };
 
-  return (
-    <form onSubmit={onSubmit}>
+  if (user) {
+    return (
+      <div>
+        <p>
+          {user.name} logged in
+          <button onClick={handleLogout}>logout</button>
+        </p>
+      </div>
+    );
+  } else return (
+    <form onSubmit={handleLoginFormSubmit}>
       <div>
         username
         <input
