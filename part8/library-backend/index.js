05cc3c756c0e760a3de51f033b36989807fab0f3
PieMenard <110061246+PieMenard@users.noreply.github.com>
2023-09-11 16:08:18 -0400

    complete 8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -115,6 +115,7 @@ const typeDefs = `
     type Query {
         authorCount: Int!,
         bookCount: Int!
+        allBooks: [Book!]!
       }
     
 `
@@ -123,6 +124,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
+    allBooks: () => books,
   }
 }
 
