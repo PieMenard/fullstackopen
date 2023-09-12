540b3a5535a56b262fd52b8dee7f9a3044914d8f
PieMenard <pinedo.m.gustavo@gmail.com>
2023-09-11 23:55:46 -0400

    complete 8.5: Books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -93,10 +93,6 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
     type Author {
         name: String!,
@@ -116,7 +112,7 @@ const typeDefs = `
     type Query {
         authorCount: Int!,
         bookCount: Int!
-        allBooks(author: String): [Book!]!
+        allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author!]!
       }
     
@@ -126,8 +122,19 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: (_root, { author }) =>
-      !author ? books : books.filter((b) => b.author === author),
+    allBooks: (root, args) => {
+      if(args.author && args.genre){
+        return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
+      }
+      else if(args.author){
+        return books.filter(b => b.author === args.author)
+      }
+      else if(args.genre){
+        return books.filter(b => b.genres.includes(args.genre))
+      }else{
+        return books
+      }
+    },
     allAuthors: () =>authors
   },
   Author: {
