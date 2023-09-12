fe4b2c638753a63a27e0bf57df2379d83957d1c5
PieMenard <pinedo.m.gustavo@gmail.com>
2023-09-12 00:02:36 -0400

    complete 8.6: Adding a book

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -115,7 +116,15 @@ const typeDefs = `
         allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author!]!
       }
-    
+
+    type Mutation {
+      addBook(
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]!
+      ): Book
+    }
 `
 
 const resolvers = {
@@ -141,6 +150,16 @@ const resolvers = {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
   },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      if(!authors.includes(book.author)){
+        authors = authors.concat({ name: book.author, id: uuid()})
+      }
+      books = books.concat(book)
+      return book
+    }
+  }
 }
 
 const server = new ApolloServer({
